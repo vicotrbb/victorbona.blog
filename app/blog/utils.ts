db4e1988be157b87830eaef782639e32400629d3
vicotrbb <victor.bona@hotmail.com>
2025-01-26 21:44:34 -0300

    fix: fixed release date time diff with date-fns

--- app/blog/utils.ts
+++ app/blog/utils.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import { formatDistanceToNow, format, parseISO } from "date-fns";
 
 type Metadata = {
   title: string;
@@ -55,39 +56,18 @@ export function getBlogPosts() {
 }
 
 export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date);
-
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-  let daysAgo = currentDate.getDate() - targetDate.getDate();
-
-  let formattedDate = "";
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`;
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`;
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`;
-  } else {
-    formattedDate = "Today";
-  }
-
-  let fullDate = targetDate.toLocaleString("en-us", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+  const parsedDate = parseISO(date);
+  const fullDate = format(parsedDate, "MMMM d, yyyy");
 
   if (!includeRelative) {
     return fullDate;
   }
 
-  return `${fullDate} (${formattedDate})`;
+  const relativeDate = formatDistanceToNow(parsedDate, { addSuffix: true });
+  return `${fullDate} (${relativeDate})`;
 }
 
 export function getReadingTime(content: string) {
